61438c84243106db41cb8d0ea8e5118ba6344795
Николай Ёлшин <nikolajelsin@Mac-mini.local>
2025-03-06 13:22:26 +0800

    test fixes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { FuncBasedApp, ClassBasedApp } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders brand title in a function-based component', () => {
+  render(<FuncBasedApp />);
+  const brandElement = screen.getByText(/Tiger of Sweden/i);
+  expect(brandElement).toBeInTheDocument();
+});
+
+test('renders brand title in a class-based component', () => {
+  render(<ClassBasedApp />);
+  const brandElement = screen.getByText(/Tiger of Sweden/i);
+  expect(brandElement).toBeInTheDocument();
 });
